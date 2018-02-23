faf826aefa10a2daec92ec21edf03a8485fd4dee
odan <odan3240@gmail.com>
2018-02-23 23:36:12 +0900

    fix sign-in layout

--- web/pages/auth/sign-in.vue
+++ web/pages/auth/sign-in.vue
@@ -21,3 +21,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  text-align: center;
+}
+
+</style>
+
